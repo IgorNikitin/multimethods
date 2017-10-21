2bcdc0ca967902fa03402d0f1157a39bdff51cb7
Igor Nikitin <igor_nikitin@valentina-db.com>
2017-10-21 21:27:49 +0300

    add bench

---
+++ benches/bench2.cpp
@@ -0,0 +1,27 @@
+#include <ctime>
+#include <iostream>
+#include "multimethods.h"
+using namespace std;
+
+struct base : public multimethods::unknown {};
+struct asteroid : public base {};
+struct spaceship : public base {};
+
+declare_method(collide)
+define_method(collide, asteroid&, asteroid&) {}
+define_method(collide, asteroid&, spaceship&) {}
+define_method(collide, spaceship&, asteroid&) {}
+define_method(collide, spaceship&, spaceship&) {}
+
+int main() {
+   asteroid a;
+   spaceship s1, s2;
+
+   clock_t begin = clock();
+
+   for(int i = 0; i < 1000000; ++i)
+       collide(static_cast<base&>(s1), static_cast<base&>(s2));
+
+    const double ms = double(clock() - begin) / CLOCKS_PER_SEC;
+    printf("%f\n", ms);
+}
