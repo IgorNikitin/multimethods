85a1755b44b2cbde82940f00e4ce49a90454e70c
Igor Nikitin <igor_nikitin@valentina-db.com>
2017-10-21 15:14:25 +0300

    update an example

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -13,14 +13,18 @@ struct spaceship_big : spaceship {
 };
 
 declare_method(collide, void)
-define_method(collide, asteroid&, spaceship&) { cout << "Boom!" << endl; }
-define_method(collide, spaceship&, const spaceship& s) { if(s.is_big()) skip_method; }
-define_method(collide, spaceship&, const spaceship_big&) { cout << "Knock, knock." << endl; }
+define_method(collide, asteroid&, spaceship_big&) { cout << "Big boom!.\n"; }
+define_method(collide, asteroid&, spaceship&) { cout << "Boom!\n"; }
+define_method(collide, spaceship&, const spaceship& s) { cout << "Knock, knock.\n"; }
 
 declare_method(concat, string)
 define_method(concat) { return ""s; }
 define_method(concat, int x, int y, int z) { return to_string(x) + to_string(y) + to_string(z); }
 define_method(concat, string s1, string s2) { return s1 + s2; }
+
+declare_method(mm_abs, int)
+define_method(mm_abs, int n) { if(n > 0) skip_method; return -n; }
+define_method(mm_abs, int n) { return n; }
 
 int main() {
    asteroid a;
@@ -28,10 +32,13 @@ int main() {
    spaceship_big bs;
 
    collide(a, s1);
-   collide(s1, s2);
+   collide(a, bs);
    collide(s2, bs);
 
    cout << concat() << endl;
    cout << concat(1, 2, 3) << endl;
    cout << concat("Hello,"s, " world."s) << endl;
+
+   cout << mm_abs( -10 ) << endl;
+   cout << mm_abs( 10 ) << endl;
 }
