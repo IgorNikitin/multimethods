c38ff7e091bb5c1edef9f63b28823e5f48ef5a91
Igor Nikitin <igor_nikitin@valentina-db.com>
2017-10-22 12:20:41 +0300

    update an example

--- examples/asteroids_poly.cpp
+++ examples/asteroids_poly.cpp
@@ -9,10 +9,9 @@ struct spaceship : thing {};
 
 declare_method(collide, string, thing)
 define_method(collide, asteroid&, asteroid&) { return "Traverse"; }
-define_method(collide, asteroid& a, bullet& b) { return collide(b, a); }
+define_method(collide, asteroid&, bullet&) { return "Hit"; }
 define_method(collide, asteroid&, spaceship&) { return "Boom"; }
-define_method(collide, bullet&, asteroid&) { return "Hit"; }
-define_method(collide, spaceship& s, asteroid& a) { return collide(a, s); }
+define_method(collide, thing& t, asteroid& a) { return collide(a, t); }
 define_method_fallback(collide) { return {}; }
 
 int main() {
