a98989d969321265567b6437c1de0427c80b4739
Igor Nikitin <igor_nikitin@valentina-db.com>
2017-10-23 17:45:16 +0300

    update an example

--- examples/third.cpp
+++ examples/third.cpp
@@ -16,18 +16,16 @@ struct business : reason {};
 struct comfort : reason {};
 
 define_method(approve, bool)
-    match(const expense&, const ceo&, const reason&) { return true; }
-    match(const cab&, const manager&, const business&) { return true; }
+    match(expense&, ceo&, reason&) { return true; }
+    match(cab&, manager&, business&) { return true; }
     fallback { return false; }
 end_method
 
 int main() {
-    cout << boolalpha;
-
     cout << approve(plane(), ceo(), comfort()) << endl; // true
     cout << approve(cab(), ceo(), business()) << endl; // true
 
     cout << approve(cab(), manager(), business()) << endl; // true
     cout << approve(cab(), manager(), comfort()) << endl; // false
-    cout << approve(plane(), manager(), comfort()) << endl; // false
+    cout << approve(plane(), manager(), business()) << endl; // false
 }
